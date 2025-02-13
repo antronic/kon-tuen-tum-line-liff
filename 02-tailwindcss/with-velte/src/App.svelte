68bfb0806f1fe9b23bbaf017b0bba2210a1f5d5e
Jirachai C <jirachai.c@outlook.com>
2025-02-14 00:23:52 +0800

    Add Tailwind with svelte

---
+++ 02-tailwindcss/with-velte/src/App.svelte
@@ -0,0 +1,81 @@
+<script>
+</script>
+
+<main class="bg-yellow-50">
+  <nav class="bg-yellow-500 border-b border-yellow-400 text-white px-4 py-2 prompt-light">
+    <div class="flex justify-between items-center container mx-auto">
+      <div></div>
+      <div class="flex space-x-4 text-lg">
+        <a href="#" class="">หน้าหลัก</a>
+        <a href="#" class="">กิจกรรม</a>
+        <a href="#" class="">ติดต่อเรา</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="bg-gradient-to-br from-yellow-500 to-amber-400 flex py-64">
+    <div class="pl-8 container mx-auto relative">
+      <h1 class="text-yellow-700 prompt-medium">สวัสดีทุกท่าน<br/>ผู้รักในการหาทำ</h1>
+      <p class="text-yellow-600 text-lg py-2">อยู่บ้านเฉย ๆ อาจน่าเบื่อ ออกหาทำกันดีกว่า<br/>ตรงนี้เขียนอะไรดีอะ...อะ ปิ่ว ปิ้ว ปิ๊ว</p>
+
+      <a href="#" class="inline-block mt-4 bg-amber-700 hover:bg-amber-600 text-amber-100 px-4 py-2 text-lg">ร่วมทำความดี 🙏</a>
+    </div>
+
+    <div class="relative w-full cursor-pointer">
+      <div class="bg-yellow-200/50 text-yellow-400 absolute transition-transform hover:scale-125 active:scale-90 right-28 bottom-4 p-8 w-1/3 rounded animate-pulse">
+        <p>ใส่รูปตรงนี้นะ</p>
+        <p class="text-sm">รูปอะไรนะ</p>
+      </div>
+
+      <div class="bg-cyan-200/60 text-yellow-400 absolute transition-transform hover:scale-125 active:scale-90 right-54 bottom-12 p-4 w-48 h-48 rounded-full animate-bounce"></div>
+      <div class="bg-rose-200/75 text-yellow-400 absolute transition-transform hover:scale-150 active:scale-90 right-72 bottom-2 p-4 w-32 h-32 rounded-xl animate-spin"></div>
+    </div>
+  </div>
+
+  <div class="py-4">
+    <div class="text-center text-slate-800">
+      <h2 class="text-amber-700 prompt-medium text-4xl!">วางแผนกิจกรรมแสนสุขสำหรับวันหยุด</h2>
+      <p>คำโปรย คำโปรย คำโปรย คำโปรย <span class="animate-pulse">✨✨</span></p>
+    </div>
+  </div>
+
+  <div class="grid grid-cols-12 gap-x-8 px-8 container mx-auto pt-4 pb-16">
+    <!-- Start region - Card -->
+    <div class="col-span-12 md:col-span-6 lg:col-span-4 bg-amber-100 rounded-lg overflow-clip hover:shadow-xl shadow-md transition-shadow">
+      <div class="">
+        <img width="100%" height="100%" src="/img01.png" alt="">
+        <div class="p-4">
+          <h3 class="text-amber-700 prompt-medium">กิจกรรม ปีนต้นงิ้ว</h3>
+          <p class="break-words text-slate-800">นานแค่ไหนแล้วนะ ที่ไม่ได้ออกกำลัง จริง ๆ จัง และดูเหมือนว่า...</p>
+        </div>
+      </div>
+    </div>
+    <!-- End region - Card -->
+    <!-- Start region - Card -->
+    <div class="col-span-12 md:col-span-6 lg:col-span-4 bg-amber-100 rounded-lg overflow-clip hover:shadow-xl shadow-md transition-shadow">
+      <div class="">
+        <img width="100%" height="100%" src="/img02.png" alt="">
+        <div class="p-4">
+          <h3 class="text-amber-700 prompt-medium text-2xl">กิจกรรม แช่น้ำร้อนทองแดง</h3>
+          <p class="break-words text-slate-800">ร่างกายอันเหนื่อยล้าที่เกิดจากใช้ชีวิตอย่างหักโหม มันช่างอึดอัดเหลือเกิน...</p>
+        </div>
+      </div>
+    </div>
+    <!-- End region - Card -->
+    <!-- Start region - Card -->
+    <div class="col-span-12 md:col-span-6 lg:col-span-4 bg-amber-100 rounded-lg overflow-clip hover:shadow-xl shadow-md transition-shadow flex items-center">
+      <div class="">
+        <div class="p-4">
+          <h3 class="text-amber-700 prompt-medium text-2xl">และกิจกรรม อีกมากมาย</h3>
+          <p class="break-words text-slate-600">รอให้ท่านมาร่วมสนุกด้วยกัน~</p>
+        </div>
+      </div>
+    </div>
+    <!-- End region - Card -->
+  </div>
+</main>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+</style>
+
