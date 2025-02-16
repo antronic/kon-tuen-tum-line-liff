d259a84900ac777fcc7f980db3fd81880f62e836
Jirachai Chansivanon <jirachai.c@outlook.com>
2025-02-16 14:59:31 +0700

    Add LINE LIFF exercise

--- 02-tailwindcss/with-sveltekit/src/routes/+page.svelte
+++ 02-tailwindcss/with-sveltekit/src/routes/+page.svelte
@@ -1,3 +1,32 @@
+<script>
+  let count = $state(0)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p class="text-red-900">123</p>
+
+
+<p>count: {count}</p>
+<button onclick={() => {
+  count++
+}}>Increment</button>
+
+<div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <h1 class="text-2xl p-8 inline-block text-red-500 font-bold underline">
+    Hello world!
+  </h1>
+  <p class="text-xl bg-amber-500 inline-block text-cyan-300 font-bold">On the right hand side</p>
+</div>
+
+<button class="rounded px-4 py-2 bg-teal-300 text-black hover:bg-red-500 active:bg-blue-500">This is button</button>
+
+<style lang="postcss">
+  @reference "tailwindcss/theme";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
---
+++ 03-use-line-liff/exercise-liff-sveltekit/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+import { onMount } from 'svelte'
+import liff from '@line/liff'
+
+let isLogin = false
+
+// On mount, check if the user is logged in
+onMount(async () => {
+  // Initialize LIFF
+
+  // Check if the user is logged in
+
+  // If the user is logged in, set isLogin to true and profile
+
+  // If the user is not logged in, login, show login button
+})
+
+</script>
+<div class="h-screen flex justify-center items-center">
+  {#if isLogin}
+    <div class="">
+      <h1 class="text-2xl">Profile:</h1>
+      <div>
+        <img src={profile.pictureUrl} alt="profile" class="w-20 h-20 rounded-full" />
+        <p>{profile.displayName}</p>
+        <p>{profile.userId}</p>
+      </div>
+    </div>
+  {:else}
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={login}>
+      Login
+    </button>
+  {/if}
+</div>
